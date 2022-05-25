e80c6f14ffa048268cc4ea25feaf76f2eaddc6e1
DeNic0la <nicola@fioretti.info>
2022-05-25 21:57:43 +0200

    Started Refactoring from TSQL to MySql II

--- Code/DEV_SELECTS.sql
+++ Code/DEV_SELECTS.sql
@@ -2,3 +2,8 @@ SELECT * FROM Person
 JOIN Worth W on Person.id = W.person_id;
 
 SELECT * FROM Worth;
+
+select * from Person
+JOIN Worth W on Person.id = W.person_id
+JOIN Executed_Punishments EP on Person.id = EP.person_id
+JOIN Punishments P on P.id = EP.punishment_id;
--- Code/payday_procedure.sql
+++ Code/payday_procedure.sql
@@ -1,12 +1,12 @@
 DROP PROCEDURE IF EXISTS sp_personalPayday;
 DELIMITER &&
 CREATE PROCEDURE sp_personalPayday (
-    IN @personId INT
+    IN var_personId INT
 )BEGIN
-    DECLARE @score DEC;
-    DECLARE @worstPunishmentId DEC;
-    SET @score = (SELECT score FROM Worth WHERE person_id = @personId);
-    SET @worstPunishmentId = (SELECT id FROM Punishments WHERE trigger_score <= @score ORDER BY id DESC LIMIT 1);
-    INSERT INTO Executed_Punishments (is_executed, person_id, punishment_id) VALUES (false, @personId, @worstPunishmentId);
+    DECLARE var_score DEC(10,2);
+    DECLARE var_worstPunishmentId DEC(10,2);
+    SET var_score = (SELECT score FROM Worth WHERE person_id = var_personId);
+    SET var_worstPunishmentId = (SELECT id FROM Punishments WHERE trigger_score <= var_score ORDER BY id DESC LIMIT 1);
+    INSERT INTO Executed_Punishments (is_executed, person_id, punishment_id) VALUES (false, var_personId, var_worstPunishmentId);
 END &&
 DELIMITER ;
