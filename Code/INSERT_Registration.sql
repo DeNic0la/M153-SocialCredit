f3b456152b5d0086627a86cddf5569186fecd3a3
DeNic0la <nicola@fioretti.info>
2022-06-01 17:07:59 +0200

    added Registrations

---
+++ Code/INSERT_Registration.sql
@@ -0,0 +1,3 @@
+INSERT INTO Person (first_name, last_name) VALUE ("Nicola","Fioretti");
+INSERT INTO Person (first_name, last_name) VALUE ("Laurin","Lötscher");
+INSERT INTO Person (first_name, last_name) VALUE ("Peter","Kaufmann");
