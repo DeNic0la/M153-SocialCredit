bbfd8a3cdbd4e8dd329e659457778c847526575c
DeNic0la <nicola@fioretti.info>
2022-06-01 16:47:58 +0200

    Added Punish Enemy

---
+++ Code/MOD_punishPublicEnemy.sql
@@ -0,0 +1,11 @@
+SET @personId := (SELECT person_id FROM Worth ORDER BY score ASC LIMIT 1);
+CALL sp_personalPayday(@personId);
+SELECT Pu.name                                  as 'Punishment Name',
+       CONCAT(Pe.first_name, ' ', Pe.last_name) as 'Criminal Name',
+       Pu.serverity                             as 'Punishment Severity',
+       ROUND(W.score, 2)                        as 'Person Worth'
+from Executed_Punishments Ep
+         JOIN Person Pe on Pe.id = Ep.person_id
+         JOIN Punishments Pu on Pu.id = Ep.punishment_id
+         JOIN Worth W on Pe.id = W.person_id
+WHERE Ep.is_executed = 0 AND Pe.id = @personId
