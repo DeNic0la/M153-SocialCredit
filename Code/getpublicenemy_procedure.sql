69ca03ce1041ec6c11570bab86680ac1930c66ef
DeNic0la <nicola@fioretti.info>
2022-06-01 16:12:44 +0200

    fixing laurins code

--- Code/getpublicenemy_procedure.sql
+++ Code/getpublicenemy_procedure.sql
@@ -4,7 +4,7 @@ DELIMITER &&
 CREATE PROCEDURE sp_getPublicEnemy()
 BEGIN
     DECLARE var_highscore DEC(10,2);
-    SET var_highscore = (SELECT person_id FROM Worth ORDER BY score DESC LIMIT 1)
-    SELECT CONCAT_WS(' ', first_name, last_name) as 'Public Enemy' FROM Person WHERE id = var_highscore
+    SET var_highscore = (SELECT person_id FROM Worth ORDER BY score ASC LIMIT 1);
+    SELECT CONCAT_WS(' ', first_name, last_name) as 'Public Enemy' FROM Person WHERE id = var_highscore;
 END &&
 DELIMITER ;
